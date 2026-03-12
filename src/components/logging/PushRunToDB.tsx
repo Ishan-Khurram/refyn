bd368dff1041e02e702605fdde752f6fca01ccba
Ishan-Khurram <mo.ishan.khurram@gmail.com>
2026-03-12 16:19:09 -0400

    merge new changes

---
+++ src/components/logging/PushRunToDB.tsx
@@ -0,0 +1,24 @@
+// The button logic to push the data for the run to firestore.
+import { db } from "../../firebase/firebase";
+import {
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  serverTimestamp,
+  Timestamp,
+} from "firebase/firestore";
+
+// temporary UID since logging isnt a thing yet
+const TEMP_UID = "testUser";
+// general folder id before adding create folder button
+const GENERAL_FOLDER_ID = "general";
+
+async function pushRunToDB() {
+  // check if general folder is there.
+  await setDoc(
+    doc(db, "users", TEMP_UID, "runFolders", GENERAL_FOLDER_ID),
+    { name: "General", isDefault: true, createdAt: serverTimestamp() },
+    { merge: true },
+  );
+}
